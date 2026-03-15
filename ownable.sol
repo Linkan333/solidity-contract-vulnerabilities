320ae6cfe992f5be09e918bd9d978cb85fb0d1a2
Linkan333 <linunybe@gmail.com>
2026-03-15 18:23:50 +0100

    Initial

--- ownable.sol
+++ ownable.sol
@@ -1,2 +1,17 @@
-// SPDX-License-Identifier: GNU
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.34;
+
+contract IsOwnable {
+  address owner;
+  constructor() {
+    _; // this is the bug, when you call this over the requirement we have the function body will run directly.
+    // this will cause that if there are admin calls for example we can execute them as owner. or set our own owner if that function is available.
+    require(msg.sender == owner, "You are not the owner of this contract");
+  }
+
+  function maybeAdminCalls() {
+    /* no code here */
+  }
+}
+
 
