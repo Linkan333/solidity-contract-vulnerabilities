7c663a95a5f53a9f19eb96069435d724b3b9a77e
Linkan333 <linunybe@gmail.com>
2026-03-15 18:25:46 +0100

    Initial

--- ownable.sol
+++ ownable.sol
@@ -7,6 +7,7 @@ contract IsOwnable {
     _; // this is the bug, when you call this over the requirement we have the function body will run directly.
     // this will cause that if there are admin calls for example we can execute them as owner. or set our own owner if that function is available.
     require(msg.sender == owner, "You are not the owner of this contract");
+    // this is not required to be an bug however when used wrong it can lead to severe consequences
   }
 
   function maybeAdminCalls() {
